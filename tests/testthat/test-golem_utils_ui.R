b4940d623b2856c82eeb6a90536b49fdd8a3ba47
Nate Delaney-Busch <ndelaneybusch@gmail.com>
2026-03-17 22:18:35 -0700

    Bugfix: new style

--- tests/testthat/test-golem_utils_ui.R
+++ tests/testthat/test-golem_utils_ui.R
@@ -93,15 +93,16 @@ test_that("Test undisplay works", {
 
   b <- shiny::actionButton("go_filter", "go")
   expect_s3_class(b, "shiny.tag")
-  expect_equal(
-    as.character(b),
-    '<button id="go_filter" type="button" class="btn btn-default action-button">go</button>'
+  # Shiny >= 1.10 wraps labels in <span class="action-label">; match either format
+  expect_true(
+    grepl('id="go_filter"', as.character(b)) &&
+    grepl("go</", as.character(b))
   )
   b_undisplay <- undisplay(b)
   expect_s3_class(b, "shiny.tag")
-  expect_equal(
-    as.character(b_undisplay),
-    '<button id="go_filter" type="button" class="btn btn-default action-button" style="display: none;">go</button>'
+  expect_true(
+    grepl('style="display: none;"', as.character(b_undisplay)) &&
+    grepl('id="go_filter"', as.character(b_undisplay))
   )
 
   c <- shiny::tags$p(src = "plop", style = "some_style", "pouet")
